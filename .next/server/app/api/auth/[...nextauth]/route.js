59e4abc9624578131dc3dcbf015f8e5e397ed660
7irelo <tirelo.eric@gmail.com>
2026-05-10 19:11:19 +0200

    chore: untrack .next build artifacts

--- .next/server/app/api/auth/[...nextauth]/route.js
+++
@@ -1,7 +0,0 @@
-var R=require("../../../../chunks/[turbopack]_runtime.js")("server/app/api/auth/[...nextauth]/route.js")
-R.c("server/chunks/[externals]__0wmg1nn._.js")
-R.c("server/chunks/[root-of-the-server]__0skxvc3._.js")
-R.c("server/chunks/[root-of-the-server]__0tx2wma._.js")
-R.c("server/chunks/0~59_ui__next-internal_server_app_api_auth_[___nextauth]_route_actions_0cieqks.js")
-R.m(219347)
-module.exports=R.m(219347).exports
--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"67EYy3R21UtE0f5n3U2RcWKO9GdNGaloaq2mkFwCUso=\"\n}"
